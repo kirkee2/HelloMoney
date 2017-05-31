e4375165307484747db219a60fea3d82ee9345b6
Joon <kirkee2@naver.com>
2017-05-31 17:37:19 +0900

    cancel custom dialog added

--- app/src/main/java/com/beta/tacademy/hellomoneycustomer/activity/RequestQuotationActivity.java
+++ app/src/main/java/com/beta/tacademy/hellomoneycustomer/activity/RequestQuotationActivity.java
@@ -1,20 +1,19 @@
 package com.beta.tacademy.hellomoneycustomer.activity;
 
-import android.graphics.Color;
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v4.content.res.ResourcesCompat;
-import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import android.view.MenuItem;
-import android.webkit.WebView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.beta.tacademy.hellomoneycustomer.R;
-import com.beta.tacademy.hellomoneycustomer.common.HelloMoneyCustomerApplication;
 
 public class RequestQuotationActivity extends AppCompatActivity {
 
@@ -40,21 +39,61 @@ public class RequestQuotationActivity extends AppCompatActivity {
 
         toolbar.setTitle(getResources().getString(R.string.request_quotation));
         toolbar.setTitleTextColor(ResourcesCompat.getColor(getApplicationContext().getResources(),R.color.normalTypo,null));
-        
+
     }
 
     //back 버튼 클릭 시 이벤트 설정.
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
+        CancelDialog cancelDialog = new CancelDialog(RequestQuotationActivity.this);
+
+
+        cancelDialog.show();
     }
 
 
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            finish();
+            CancelDialog cancelDialog = new CancelDialog(RequestQuotationActivity.this);
+
+
+            cancelDialog.show();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class CancelDialog extends Dialog {
+
+        Button no;
+        Button yes;
+        public CancelDialog(@NonNull Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.cancel_custom_dialog);
+
+            yes = (Button)findViewById(R.id.yes);
+            no = (Button)findViewById(R.id.no);
+
+            no.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+
+            yes.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                    dismiss();
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/beta/tacademy/hellomoneycustomer/recyclerViews/RequestQuotationRecyclerView/RequestQuotationValueObject.java
@@ -0,0 +1,8 @@
+package com.beta.tacademy.hellomoneycustomer.recyclerViews.RequestQuotationRecyclerView;
+
+/**
+ * Created by kirkee on 2017. 5. 31..
+ */
+
+public class RequestQuotationValueObject {
+}
